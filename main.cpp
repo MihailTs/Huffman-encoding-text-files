ac83fcd0ce5816fb41e62cd17a8098c187301b74
MihailTs <mihail.tsvetkov.04@gmail.com>
2023-12-14 00:42:25 +0200

    User interface

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include "CommandProcessing.h"
 #include "HuffmanEncoder.h"
 
 int main(){
 
     HuffmanEncoder encoder("testInputFile.txt", "testOutputFile.txt");
-    //HuffmanEncoder decoder("testOutputFile.txt", "testInputFile.txt");
+    
+    std::string command;
 
-    encoder.compress();
-    //decoder.decompress();
-    encoder.debugRegime();
+    while(true){
+        std::cout << "> ";
+        std::getline(std::cin, command);
 
-    std::cout << encoder.getCompressionDegree();
+        command = strip(command);
+
+        if(command == "c"){
+            encoder.compress();
+        } else if(command == "d"){
+            encoder.decompress();
+        } else if(command.substr(0, 2) == "i "){
+            encoder.setInputFile(command.substr(2));
+        } else if(command.substr(0, 2) == "o "){
+            encoder.setOutputFile(command.substr(2));
+        } else if(command == "debug"){
+            encoder.debugRegime();
+        } else if(command == "info"){
+            std::cout << "The last compression had " << encoder.getCompressionDegree()
+                            << " efficiency. (input bits / output bits ratio)" << std::endl;
+        } else if(command == "exit"){
+            std::cout << "Bye!";
+            break;
+        }
+
+    }
 
     return 0;
 }
